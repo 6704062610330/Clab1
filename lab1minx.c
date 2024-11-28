c83b8d5bd8204d46d0d6474a06523066ceb2f5e6
6704062610330 <s6704062610330@email.kmutnb.ac.th>
2024-11-28 14:21:28 +0700

    update

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main()
 {
-	int num1=9;
-	int num2=9;
+	int num1=56;
+	int num2=30;
 	if (num1>num2){
 		printf("num1 > num2");
-	}
-	if (num2>num1){
+	}else{
 		printf("num2 > num1");
 	}
 }
